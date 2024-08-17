27b88d4b4f52f04e4ab2ec7b2e1f6cb822701182
jaymesakae <jayme.furyama@gmail.com>
2024-08-17 08:52:35 -0300

    Adicionando argparse main.py

--- main.py
+++ main.py
@@ -5,14 +5,27 @@ from data import OxfordPetsDataset
 from model import load_resnet18
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--batch_size", help="Amount of batch size",
+                    default=64, type=int)
+parser.add_argument("--device", help="Device where run program, eg 'cuda' or 'cpu'",
+                    default='cuda', type=str)
+parser.add_argument("--num_processes", 
+                    help="Amount of cores of cpu to run program",
+                    default=4, type=int)
+args = parser.parse_args()
 
 
 # TODO: adicionar ArgParser para definir hyperparams em CLI
 if __name__ == '__main__':
-    
-    BATCH_SIZE = 64
-    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    NUM_PROCESSES = 4
+    BATCH_SIZE = args.batch_size
+    DEVICE = args.device
+    NUM_PROCESSES = args.num_processes
+    # BATCH_SIZE = 64
+    # DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # NUM_PROCESSES = 4
 
 
     data_transforms = OxfordPetsDataset.imagenet_transforms()
