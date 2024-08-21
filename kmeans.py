5215f16d45d77c448024a4f9e46a5ae5d7b2ee6e
jimiHendrix <vinicius.perillo25@gmail.com>
2024-08-21 19:41:29 -0300

    ref: refatoracao pra arrumar salvamento de codigo

--- kmeans.py
+++ kmeans.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandas.core.groupby.generic import DataFrameGroupBy
+import numpy as np
 
 import os
 import shutil
@@ -7,7 +8,7 @@ import shutil
 from joblib import Parallel, delayed
 
 from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score, davies_bouldin_score
+from sklearn.metrics import silhouette_score, adjusted_rand_score
 
 from kmeans_plots import *
   
@@ -19,9 +20,7 @@ def _compute_scores(model: KMeans, features: pd.DataFrame):
 
 
 def test_k():
-    data = pd.read_csv('features.csv')
-    
-    features = data.iloc[:,:512]
+    features = np.load("./dataset/extracted-features/images.npy") 
 
     silhouette_coefficients = []
 
@@ -29,7 +28,6 @@ def test_k():
 
     for k in range(2,38):
         sc_mean = 0.0
-        db_mean = 0.0
 
         model = KMeans(k, init= 'k-means++', n_init= 'auto')
 
@@ -39,8 +37,7 @@ def test_k():
         
         silhouette_coefficients.append(sc_mean/t)
 
-    pd.DataFrame({'silhouette': silhouette_coefficients}).to_csv('clustering_scores.csv', index= False)
-    return pd.Series(silhouette_coefficients)
+    pd.DataFrame({'silhouette': silhouette_coefficients}).to_csv('./dataset/extracted-features/clustering_scores.csv', index= False)
 
 def __copy_image(file: str, cluster: int, metric: str):
     shutil.copy(f'./{file}', f'{file.replace("oxford-iiit-pet/images", f"clusters/{metric}/c{cluster}")}')
@@ -56,19 +53,25 @@ def _create_clusters_image_folders(data: pd.DataFrame, metric: str):
 
         group['image_path'].apply(__copy_image, args= [cluster, metric])
 
-def _create_log(data: DataFrameGroupBy, k: int):
-    total = data['breed'].value_counts()
+def _create_log(data: DataFrameGroupBy, k: int, metric: str):
+    total1 = data['breed'].value_counts()
+    total2 = data['species_id'].map({1: 'dog', 0: 'cat'}).value_counts()
     
     if not os.path.exists('./log'):
         os.mkdir('./log')
 
-    with open('logs/silhouete.log', 'w') as sil:
+    with open(f'logs/{metric}.log', 'w') as sil:
         for i in range(k):
-            print_data = pd.DataFrame(dict(percentage=((data.loc[(i)]['breed'].value_counts() / total).dropna()*100),
+            print_data1 = pd.DataFrame(dict(percentage=((data.loc[(i)]['breed'].value_counts() / total1).dropna()*100),
                                         count= data.loc[(i)]['breed'].value_counts())).sort_values(by='percentage', ascending= False)
             
+            print_data2 = pd.DataFrame(dict(percentage=((data.loc[(i)]['species_id'].map({1: 'dog', 0: 'cat'}).value_counts() / total2).dropna()*100),
+                                        count= data.loc[(i)]['species_id'].map({1: 'dog', 0: 'cat'}).value_counts())).sort_values(by='percentage', ascending= False)
+            
             print(f'Cluster: {i}', file= sil, flush=True)
-            print(print_data, file= sil, flush=True)
+            print(print_data1, file= sil, flush=True)
+            print('', file= sil, flush=True)
+            print(print_data2, file= sil, flush=True)
             print('', file= sil, flush=True)
             print('', file= sil, flush=True)
 
@@ -89,9 +92,8 @@ def best_k(scores: pd.Series) -> dict:
     return k_param
 
 def analise_clusters(scores: pd.Series):
-    data = pd.read_csv('features.csv')
-    features = data.iloc[:,:512]
-    labels = data.iloc[:,512:] 
+    features = np.load("./dataset/extracted-features/images.npy")
+    labels = pd.read_csv("./dataset/extracted-features/labels.csv")
     k_param = best_k(scores)
 
     for metric, k in k_param.items():
@@ -99,18 +101,25 @@ def analise_clusters(scores: pd.Series):
         labels[metric] = model.fit_predict(features)
         
 
-        group = labels.groupby(metric).apply(lambda x: x, include_groups=False)
+        group = labels.groupby(metric, group_keys= True).apply(lambda x: x)
         group['breed'] = group['name'].str.rsplit('_', expand=True, n= 1)[0]
 
-        _create_log(group, k)
-        # _create_clusters_image_folders(group, metric)
+        _create_log(group, k, metric)
+        _create_clusters_image_folders(group, metric)
+
+        labels.to_csv(f'./dataset/clusters/{metric}.csv')
 
         if metric == 'silhouette':
             silhouette_plot(model, group, features)
 
+        print(f'K-Means com k = {k}')
+        print(f'Ajusted Rand Score para species: {adjusted_rand_score(labels["species_id"], labels[metric])}')
+        print(f'Ajusted Rand Score para species: {adjusted_rand_score(labels["breed_id"], labels[metric])}')
+        print()
+
 
 def main():
-    scores = test_k()
+    scores = pd.read_csv('clustering_scores.csv')['silhouette']
     analise_clusters(scores)
 
 main()
